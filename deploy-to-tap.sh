087e5cc52d0797795588d3167eea68d8b828c803
Mark Fynes <tfynes@vmware.com>
2022-04-20 15:00:43 -0400

    deploy to tap after cloning

---
+++ deploy-to-tap.sh
@@ -0,0 +1,6 @@
+kubectl apply -f ./src/products/workload.yaml
+kubectl apply -f ./src/api-gateway/workload.yaml
+kubectl apply -f ./src/checkout/workload.yaml
+kubectl apply -f ./src/cart/workload.yaml
+kubectl apply -f ./src/eureka-server-local/workload.yaml
+kubectl apply -f ./src/login/workload.yaml
