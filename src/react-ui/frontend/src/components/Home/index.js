af94c54ddd0b3fc1093bc6c0cb34ff4dc40cb012
tfynes-pivotal <tfynes@pivotal.io>
2022-05-19 15:34:13 -0400

    Update index.js

--- src/react-ui/frontend/src/components/Home/index.js
+++ src/react-ui/frontend/src/components/Home/index.js
@@ -20,7 +20,7 @@ class Home extends Component {
       <div>
         <Hero/>
         <div className="paragraph">
-          <Products addItemToCart={this.props.addItemToCart} isInline={true} name={<span>Bestsellers in <Link to={"/Books"}>Books</Link></span>} category="Books" limit={4}/>
+          <Products addItemToCart={this.props.addItemToCart} isInline={true} name={<span>Top Sellers in <Link to={"/Books"}>Books</Link></span>} category="Books" limit={4}/>
           <Products addItemToCart={this.props.addItemToCart} isInline={true} name={<span>Bestsellers in <Link to={"/Music"}>Music</Link></span>} category="Music" limit={4}/>
           <Products addItemToCart={this.props.addItemToCart} isInline={true} name={<span>Bestsellers in <Link to={"/Beauty"}>Beauty</Link></span>} category="Beauty" limit={4}/>
           <Products addItemToCart={this.props.addItemToCart} isInline={true} name={<span>Bestsellers in <Link to={"/Electronics"}>Electronics</Link></span>} category="Electronics" limit={4}/>
