f1cbd8dc4549b355512cd31904e655871e29a8aa
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-08-24 14:59:39 +0800

    Enable openfeign

--- src/cart/src/main/java/com/yugabyte/app/yugastore/cart/YugastoreCart.java
+++ src/cart/src/main/java/com/yugabyte/app/yugastore/cart/YugastoreCart.java
@@ -2,8 +2,11 @@ package com.yugabyte.app.yugastore.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class YugastoreCart {
 
 	public static void main(String[] args) {
