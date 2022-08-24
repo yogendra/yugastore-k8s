dcf63b587829f0baad21558996d4835dc6fe9166
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-08-24 19:58:10 +0800

    Adding default url

--- src/checkout/src/main/java/com/yugabyte/app/yugastore/cronoscheckoutapi/rest/clients/ShoppingCartRestClient.java
+++ src/checkout/src/main/java/com/yugabyte/app/yugastore/cronoscheckoutapi/rest/clients/ShoppingCartRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("cart-microservice")
+@FeignClient(value = "cart-microservice", url="http://cart-default.tap.yuga.ga")
 public interface ShoppingCartRestClient {
 
   @RequestMapping("/cart-microservice/shoppingCart/addProduct")
