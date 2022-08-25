e6097b47ab29cd2f50c9a56c72fb2e07cef35c0a
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-08-26 01:52:59 +0800

    Cleanup

--- src/products/src/main/java/com/yugabyte/app/yugastore/YugastoreProducts.java
+++ src/products/src/main/java/com/yugabyte/app/yugastore/YugastoreProducts.java
@@ -2,13 +2,11 @@ package com.yugabyte.app.yugastore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
 public class YugastoreProducts {
 	public static void main(String[] args) {
 		SpringApplication.run(YugastoreProducts.class, args);
