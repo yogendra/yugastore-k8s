569cf701f0f18a0a7bc8b5bcfea6e5b0b11d06d5
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-08-26 01:52:16 +0800

    Removed unwanted config

--- src/checkout/src/main/java/com/yugabyte/app/yugastore/cronoscheckoutapi/YugastoreCheckout.java
+++ src/checkout/src/main/java/com/yugabyte/app/yugastore/cronoscheckoutapi/YugastoreCheckout.java
@@ -2,11 +2,9 @@ package com.yugabyte.app.yugastore.cronoscheckoutapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
 public class YugastoreCheckout {
 
