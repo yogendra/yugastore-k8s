fb50fa3baeb5f451c1cb461fd237472cb4f385f5
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-08-27 13:32:29 +0800

    removed authentication from app

--- src/checkout/src/main/java/com/yugabyte/app/yugastore/cronoscheckoutapi/YugastoreCheckout.java
+++ src/checkout/src/main/java/com/yugabyte/app/yugastore/cronoscheckoutapi/YugastoreCheckout.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+ "org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration",
+  "org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+})
 @EnableFeignClients
 public class YugastoreCheckout {
 
